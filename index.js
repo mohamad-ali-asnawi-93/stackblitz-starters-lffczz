da387042c5bcdfa05c2817659686a7ec9ce6b25b
mohamad-ali-asnawi-93 <mohamad.ali.asnawi.93@gmail.com>
2024-10-06 09:30:16 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// run `node index.js` in the terminal
+
+// don't remove this line if are using prompt
+const prompt = require('prompt-sync')();
+
+const age = prompt ("what is your age? ")
+console.log(age)
+console.log(`My age is ${age}`)
+
+const birthday = prompt("What is your birthday? ")
+console.log(birthday)
+console.log(`My birthday is on ${birthday}`)
